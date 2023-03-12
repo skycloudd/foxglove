9d145ff701f832f615bd73d2a1c5ac234613e05e
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2023-03-12 21:09:02 +0100

    refactor ast lowering functions

--- src/lower.rs
+++ src/lower.rs
@@ -14,7 +14,7 @@ pub fn lower(ast: &Spanned<Ast>) -> Spanned<Hir> {
                     .0
                     .clone()
                     .into_iter()
-                    .map(|item| lower_item(&item))
+                    .map(|item| lower_item(item))
                     .collect(),
                 ast.1.clone(),
             ),
@@ -23,20 +23,16 @@ pub fn lower(ast: &Spanned<Ast>) -> Spanned<Hir> {
     )
 }
 
-fn lower_item(item: &Spanned<ast::Item>) -> Spanned<Item> {
+fn lower_item(item: Spanned<ast::Item>) -> Spanned<Item> {
     (
         match item.0 {
-            ast::Item::Fn {
-                ref attrs,
-                ref sig,
-                ref body,
-            } => Item::Fn {
+            ast::Item::Fn { attrs, sig, body } => Item::Fn {
                 attrs: (
                     attrs
                         .0
                         .clone()
                         .into_iter()
-                        .map(|attr| lower_attribute(&attr))
+                        .map(|attr| lower_attribute(attr))
                         .collect(),
                     attrs.1.clone(),
                 ),
@@ -48,75 +44,69 @@ fn lower_item(item: &Spanned<ast::Item>) -> Spanned<Item> {
     )
 }
 
-fn lower_attribute(attr: &Spanned<ast::Attribute>) -> Spanned<Attribute> {
+fn lower_attribute(attr: Spanned<ast::Attribute>) -> Spanned<Attribute> {
     (
         Attribute {
-            name: lower_ident(&attr.0.name),
-            value: lower_expr(&attr.0.value),
+            name: lower_ident(attr.0.name),
+            value: lower_expr(attr.0.value),
         },
         attr.1.clone(),
     )
 }
 
-fn lower_function_signature(sig: &Spanned<ast::FunctionSignature>) -> Spanned<FunctionSignature> {
+fn lower_function_signature(sig: Spanned<ast::FunctionSignature>) -> Spanned<FunctionSignature> {
     (
         FunctionSignature {
-            name: lower_ident(&sig.0.name),
+            name: lower_ident(sig.0.name),
             params: (
                 sig.0
                     .params
                     .0
                     .clone()
                     .into_iter()
-                    .map(|param| lower_param(&param))
+                    .map(|param| lower_param(param))
                     .collect(),
                 sig.0.params.1.clone(),
             ),
-            ret_ty: lower_type(&sig.0.ret_ty),
+            ret_ty: lower_type(sig.0.ret_ty),
         },
         sig.1.clone(),
     )
 }
 
-fn lower_param(param: &Spanned<ast::Param>) -> Spanned<Param> {
+fn lower_param(param: Spanned<ast::Param>) -> Spanned<Param> {
     (
         Param {
-            name: lower_ident(&param.0.name),
-            ty: lower_type(&param.0.ty),
+            name: lower_ident(param.0.name),
+            ty: lower_type(param.0.ty),
         },
         param.1.clone(),
     )
 }
 
-fn lower_statement(stmt: &Spanned<ast::Statement>) -> Spanned<Statement> {
+fn lower_statement(stmt: Spanned<ast::Statement>) -> Spanned<Statement> {
     (
         match stmt.0 {
-            ast::Statement::Block(ref stmts) => Statement::Block((
-                stmts.0.iter().map(lower_statement).collect(),
+            ast::Statement::Block(stmts) => Statement::Block((
+                stmts.0.into_iter().map(lower_statement).collect(),
                 stmts.1.clone(),
             )),
-            ast::Statement::Expr(ref expr) => Statement::Expr(lower_expr(expr)),
-            ast::Statement::VarDecl(ref name, ref ty, ref expr) => Statement::VarDecl(
-                lower_ident(name),
-                ty.as_ref().map(lower_type),
-                lower_expr(expr),
-            ),
-            ast::Statement::Assign(ref target, ref expr) => {
+            ast::Statement::Expr(expr) => Statement::Expr(lower_expr(expr)),
+            ast::Statement::VarDecl(name, ty, expr) => {
+                Statement::VarDecl(lower_ident(name), ty.map(lower_type), lower_expr(expr))
+            }
+            ast::Statement::Assign(target, expr) => {
                 Statement::Assign(lower_assignment_target(target), lower_expr(expr))
             }
-            ast::Statement::Return(ref expr) => Statement::Return(expr.as_ref().map(lower_expr)),
-            ast::Statement::IfElse {
-                ref cond,
-                ref then,
-                ref else_,
-            } => Statement::IfElse {
+            ast::Statement::Return(expr) => Statement::Return(expr.map(lower_expr)),
+            ast::Statement::IfElse { cond, then, else_ } => Statement::IfElse {
                 cond: lower_expr(cond),
-                then: Box::new(lower_statement(then)),
-                else_: else_.as_ref().map(|else_| Box::new(lower_statement(else_))),
+                then: Box::new(lower_statement(*then)),
+                else_: else_.map(|else_| Box::new(lower_statement(*else_))),
             },
-            ast::Statement::While { ref cond, ref body } => {
+            ast::Statement::While { cond, body } => {
                 let cond = lower_expr(cond);
-                let body = lower_statement(body);
+                let body = lower_statement(*body);
 
                 let if_true_break_else_body = Statement::IfElse {
                     cond: (
@@ -137,15 +127,15 @@ fn lower_statement(stmt: &Spanned<ast::Statement>) -> Spanned<Statement> {
                 Statement::Loop(Box::new((if_true_break_else_body, stmt.1.clone())))
             }
             ast::Statement::For {
-                ref var,
-                ref start,
-                ref end,
-                ref body,
+                var,
+                start,
+                end,
+                body,
             } => {
                 let var = lower_ident(var);
                 let start = lower_expr(start);
                 let end = lower_expr(end);
-                let body = lower_statement(body);
+                let body = lower_statement(*body);
 
                 let loop_body = Statement::IfElse {
                     cond: (
@@ -210,36 +200,33 @@ fn lower_statement(stmt: &Spanned<ast::Statement>) -> Spanned<Statement> {
             }
             ast::Statement::Break => Statement::Break,
             ast::Statement::Continue => Statement::Continue,
-            ast::Statement::Loop(ref stmt) => Statement::Loop(Box::new(lower_statement(stmt))),
+            ast::Statement::Loop(stmt) => Statement::Loop(Box::new(lower_statement(*stmt))),
         },
         stmt.1.clone(),
     )
 }
 
-fn lower_expr(expr: &Spanned<ast::Expr>) -> Spanned<Expr> {
+fn lower_expr(expr: Spanned<ast::Expr>) -> Spanned<Expr> {
     (
         match expr.0 {
             ast::Expr::Error => unreachable!(),
-            ast::Expr::Literal(ref l) => Expr::Literal(lower_literal(l)),
-            ast::Expr::Var(ref name) => Expr::Var(lower_ident(name)),
-            ast::Expr::List(ref exprs) => {
-                Expr::List((exprs.0.iter().map(lower_expr).collect(), exprs.1.clone()))
-            }
-            ast::Expr::Binary {
-                ref lhs,
-                ref op,
-                ref rhs,
-            } => Expr::Binary {
-                lhs: Box::new(lower_expr(lhs)),
+            ast::Expr::Literal(l) => Expr::Literal(lower_literal(l)),
+            ast::Expr::Var(name) => Expr::Var(lower_ident(name)),
+            ast::Expr::List(exprs) => Expr::List((
+                exprs.0.into_iter().map(lower_expr).collect(),
+                exprs.1.clone(),
+            )),
+            ast::Expr::Binary { lhs, op, rhs } => Expr::Binary {
+                lhs: Box::new(lower_expr(*lhs)),
                 op: lower_binary_op(op),
-                rhs: Box::new(lower_expr(rhs)),
+                rhs: Box::new(lower_expr(*rhs)),
             },
-            ast::Expr::Prefix { ref op, ref expr } => Expr::Prefix {
+            ast::Expr::Prefix { op, expr } => Expr::Prefix {
                 op: lower_prefix_op(op),
-                expr: Box::new(lower_expr(expr)),
+                expr: Box::new(lower_expr(*expr)),
             },
-            ast::Expr::Postfix { ref expr, ref op } => Expr::Postfix {
-                expr: Box::new(lower_expr(expr)),
+            ast::Expr::Postfix { expr, op } => Expr::Postfix {
+                expr: Box::new(lower_expr(*expr)),
                 op: lower_postfix_op(op),
             },
         },
@@ -247,7 +234,7 @@ fn lower_expr(expr: &Spanned<ast::Expr>) -> Spanned<Expr> {
     )
 }
 
-fn lower_binary_op(op: &Spanned<ast::BinaryOp>) -> Spanned<BinaryOp> {
+fn lower_binary_op(op: Spanned<ast::BinaryOp>) -> Spanned<BinaryOp> {
     (
         match op.0 {
             ast::BinaryOp::Add => BinaryOp::Add,
@@ -265,7 +252,7 @@ fn lower_binary_op(op: &Spanned<ast::BinaryOp>) -> Spanned<BinaryOp> {
     )
 }
 
-fn lower_prefix_op(op: &Spanned<ast::PrefixOp>) -> Spanned<PrefixOp> {
+fn lower_prefix_op(op: Spanned<ast::PrefixOp>) -> Spanned<PrefixOp> {
     (
         match op.0 {
             ast::PrefixOp::Pos => PrefixOp::Pos,
@@ -275,15 +262,16 @@ fn lower_prefix_op(op: &Spanned<ast::PrefixOp>) -> Spanned<PrefixOp> {
     )
 }
 
-fn lower_postfix_op(op: &Spanned<ast::PostfixOp>) -> Spanned<PostfixOp> {
+fn lower_postfix_op(op: Spanned<ast::PostfixOp>) -> Spanned<PostfixOp> {
     (
         match op.0 {
             ast::PostfixOp::Error => unreachable!(),
-            ast::PostfixOp::Call(ref exprs) => {
-                PostfixOp::Call((exprs.0.iter().map(lower_expr).collect(), exprs.1.clone()))
-            }
-            ast::PostfixOp::Index(ref expr) => {
-                let expr = Box::new(lower_expr(expr));
+            ast::PostfixOp::Call(exprs) => PostfixOp::Call((
+                exprs.0.into_iter().map(lower_expr).collect(),
+                exprs.1.clone(),
+            )),
+            ast::PostfixOp::Index(expr) => {
+                let expr = Box::new(lower_expr(*expr));
                 PostfixOp::Index(expr)
             }
         },
@@ -291,7 +279,7 @@ fn lower_postfix_op(op: &Spanned<ast::PostfixOp>) -> Spanned<PostfixOp> {
     )
 }
 
-fn lower_literal(lit: &Spanned<ast::Literal>) -> Spanned<Literal> {
+fn lower_literal(lit: Spanned<ast::Literal>) -> Spanned<Literal> {
     (
         match lit.0 {
             ast::Literal::Unit => Literal::Unit,
@@ -303,17 +291,17 @@ fn lower_literal(lit: &Spanned<ast::Literal>) -> Spanned<Literal> {
     )
 }
 
-fn lower_ident(ident: &Spanned<ast::Ident>) -> Spanned<Ident> {
+fn lower_ident(ident: Spanned<ast::Ident>) -> Spanned<Ident> {
     ident.clone()
 }
 
-fn lower_assignment_target(target: &Spanned<ast::AssignmentTarget>) -> Spanned<AssignmentTarget> {
+fn lower_assignment_target(target: Spanned<ast::AssignmentTarget>) -> Spanned<AssignmentTarget> {
     (
         match target.0 {
-            ast::AssignmentTarget::Var(ref i) => AssignmentTarget::Var(lower_ident(i)),
-            ast::AssignmentTarget::Index(ref target, ref expr) => {
-                let target = lower_assignment_target(target);
-                let expr = lower_expr(expr);
+            ast::AssignmentTarget::Var(i) => AssignmentTarget::Var(lower_ident(i)),
+            ast::AssignmentTarget::Index(target, expr) => {
+                let target = lower_assignment_target(*target);
+                let expr = lower_expr(*expr);
 
                 AssignmentTarget::Index(Box::new(target), Box::new(expr))
             }
@@ -322,15 +310,15 @@ fn lower_assignment_target(target: &Spanned<ast::AssignmentTarget>) -> Spanned<A
     )
 }
 
-fn lower_type(ty: &Spanned<ast::Type>) -> Spanned<Type> {
+fn lower_type(ty: Spanned<ast::Type>) -> Spanned<Type> {
     (
         match ty.0 {
             ast::Type::Unit => Type::Unit,
-            ast::Type::Ident(ref i) => Type::Ident(lower_ident(i)),
+            ast::Type::Ident(i) => Type::Ident(lower_ident(i)),
             ast::Type::Int => Type::Int,
             ast::Type::Float => Type::Float,
             ast::Type::Bool => Type::Bool,
-            ast::Type::List(ref l) => Type::List(Box::new(lower_type(l))),
+            ast::Type::List(l) => Type::List(Box::new(lower_type(*l))),
         },
         ty.1.clone(),
     )
